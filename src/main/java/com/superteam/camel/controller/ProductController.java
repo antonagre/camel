0e182de62c9e06e3feb37aaca17834d38256a939
antonio <antonagre@gmail.com>
2021-03-04 17:14:51 +0100

    000

---
+++ src/main/java/com/superteam/camel/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.superteam.camel.controller;
+
+import com.superteam.camel.ProductCommand;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/camel/products")
+public class ProductController {
+
+    @GetMapping("/crea")
+    public String creaProdotto1(@RequestParam String name) {
+        System.out.println(name);
+        return name;
+
+    }
+
+    @GetMapping("/crea/{name}")
+    public String creaProdotto(@PathVariable String name) {
+        System.out.println(name);
+        return name;
+    }
+    @GetMapping("/crea/")
+    public String creaProdotto2(@RequestBody ProductCommand p) {
+        System.out.println(p.getName());
+        return "200";
+
+    }
+
+
+
+
+}
