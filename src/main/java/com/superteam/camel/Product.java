42d3ca3cd61d631886036acfc88a3b3c7af6ed29
Roberto Pullia <robertopullia@gmail.com>
2021-03-04 17:59:47 +0100

    Nothing to say

--- src/main/java/com/superteam/camel/Product.java
+++ src/main/java/com/superteam/camel/Product.java
@@ -1,5 +1,13 @@
 package com.superteam.camel;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+@Data
+@Builder
+
 public class Product {
     String name;
     String ref;
