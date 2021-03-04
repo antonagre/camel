f05dc74e23627b0562d70e247a127ce78f966463
Roberto Pullia <robertopullia@gmail.com>
2021-03-04 18:00:12 +0100

    Nothing to say

--- src/main/java/com/superteam/camel/ProductRepository.java
+++ src/main/java/com/superteam/camel/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.superteam.camel;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Collection;
+
 
 public interface ProductRepository extends MongoRepository<Product, ObjectId> {
 
